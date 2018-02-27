a05d5ccec96361cd0bb3bc67106b9294fa5741a7
Bryan C <bcavocado25@gmail.com>
2018-02-26 22:05:49 -0500

    Update Main.py a lot

--- Google Code Jam/Main.py
+++ Google Code Jam/Main.py
@@ -6,9 +6,26 @@ should return a string
 
 An automatic "Case #n:" will be added
 """
+# Stuff for importing modules/files
+import os
+import importlib
+os.system("clear")
+print("Files must be in the same dir as Main.py")
+
+modules = []
+for obj in os.listdir():
+    if obj.endswith(".py") and obj != "Main.py":
+        modules.append(obj)
+        print("[{}] - {}".format(len(modules) - 1, modules[-1]))
+
+selected = int(input("> "))
+if selected < 0 or selected >= len(modules):
+    print("Error - must be greater or equal to 0 and less than {}".format(len(modules)))
+    exit()
+# Now import
+SolveCase = importlib.import_module(modules[selected].replace(".py", "")).SolveCase
 # Change the name of the module to change to different solvers:
-from GCJ_2015_Qual_A import SolveCase
-FILENAME = "A-large-practice.in"
+FILENAME = "FILENAME HERE.in"
 
 f = open(FILENAME)
 cases = int(f.readline())
