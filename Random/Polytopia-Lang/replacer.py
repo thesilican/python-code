5bdb99e3127e8a9598412dd650046b7cc93da54e
MrSiliconGuy <bcavocado25@gmail.com>
2020-02-16 17:44:18 -0500

    UwU lang

--- Random/Polytopia-Lang/replacer.py
+++ Random/Polytopia-Lang/replacer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import json
+import random
 obj = json.loads(open("en_US.json", "r").read())
 
 for key, val in obj.items():
-    val = val.replace("o", "owo") \
-        .replace("u", "uwu")\
-        .replace("O", "OwO")\
-        .replace("U", "UwU")
-    val += " nya"
-    obj[key] = val
+    # val = val.replace("o", "owo") \
+    #     .replace("u", "uwu")\
+    #     .replace("O", "OwO")\
+    #     .replace("U", "UwU")
+    # val += " nya"
+    # obj[key] = val
+    words = ["UwU", "OwO", "Nya"]
+    obj[key] = random.choice(words)
 
 obj["language"] = "Languwuage"
 
-open("lang.json", "w").write(json.dumps(obj))
+open("lang1.json", "w").write(json.dumps(obj))
