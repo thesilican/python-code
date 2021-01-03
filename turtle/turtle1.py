508f43af0e8bef4d3979cc4f7a76c6387dc4b149
Bryan Chen <bryanchen74@gmail.com>
2021-01-03 03:19:44 +0000

    changes

--- turtle/turtle1.py
+++ turtle/turtle1.py
@@ -1,11 +1,28 @@
-from turtle import *
+import turtle
+import random
 
-color("red", "yellow")
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+start_points = [
+    (0, 0),
+    (100, 0),
+    (50, 86.6)
+]
+
+pos = (1, 1)
+
+turtle.penup()
+turtle.screensize(120, 120)
+turtle.setworldcoordinates(-10, -10, 110, 110)
+
+for point in start_points:
+    turtle.goto(point)
+    turtle.dot()
+
+for i in range(10000):
+    toward = random.choice(start_points)
+    pos = (
+        (pos[0] + toward[0]) / 2,
+        (pos[1] + toward[1]) / 2
+    )
+    turtle.goto(pos)
+    turtle.dot()
+turtle.done()
